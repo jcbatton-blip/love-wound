89dccc2623fb6f8859236201108e737fe6b48632
jcbatton-blip <jcbatton@gmail.com>
2026-03-27 12:53:42 -0600

    Update headshot to new B&W professional photo

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -17,7 +17,7 @@ export default function About() {
           >
             <div className="aspect-[3/4] rounded-2xl overflow-hidden shadow-xl bg-muted">
               <img 
-                src="https://i.imgur.com/Q5JZ03o.png"
+                            src="/images/headshot.jpg"
                 alt="Jeff Batton - Root-Work Mentor | Relational Practice Guide | Inner Transformation Specialist" 
                 className="w-full h-full object-cover"
               />
